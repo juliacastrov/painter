f53c37aa1119e202847c27f30300aa6589d22571
Juliana Castro V <julianacastrovar@gmail.com>
2020-04-06 12:09:44 -0500

    Add files via upload

---
+++ painter.js
@@ -0,0 +1,31 @@
+const canvas = document.querySelector("canvas")
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+const context = canvas.getContext("2d")
+
+context.strokeStyle = "blue"
+context,lineWidth = 10
+context.lineCap = "round"
+
+let shouldPaint = false
+
+document.addEventListener("mousedown", function(event){
+	shouldPaint = true
+	context.moveTo(event.pageX,event.pageY)
+	context.beginPath()
+})
+
+document.addEventListener("mouseup", function(event){
+	shouldPaint = false
+})
+
+document.addEventListener("mousemove",function(event){
+	if (shouldPaint) {
+ 	context.lineTo(event.pageX,event.pageY)
+ 	context.stroke()
+
+
+}
+})
